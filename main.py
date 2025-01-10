51d2796ead481f742c39d837418bb4be2e2eb3e7
BronSkien <91161596+BronSkien@users.noreply.github.com>
2025-01-09 21:57:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-#! /usr/bin/env python
-
-import delete_duplicate_string as dupl
-import delete_service_data as srv
 import work_with_files as files
-
+from delete_service_data import clean_service_data
+from delete_duplicate_string import delete_duplicate_lines
 
 def main():
     input_file_name = files.get_input_file_name()
     output_file_name = files.get_output_file_name()
-    text = files.read_file(input_file_name)
-    output = srv.delete_extra_service_data(text)
-    output = dupl.delete_duplicate_lines(output)
-    files.write_to_file(output, output_file_name)
-
+    
+    # Read the input file
+    lines = files.read_file(input_file_name)
+    
+    # Clean service data
+    cleaned_lines = clean_service_data(lines)
+    
+    # Remove duplicate lines
+    final_lines = delete_duplicate_lines(cleaned_lines)
+    
+    # Write to the output file
+    files.write_to_file(final_lines, output_file_name)
 
 if __name__ == "__main__":
     main()
