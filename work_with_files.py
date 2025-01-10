5555e16a007349de6fc287205d204e2d880cffbc
BronSkien <91161596+BronSkien@users.noreply.github.com>
2025-01-09 21:56:16 -0500

    Update work_with_files.py

--- work_with_files.py
+++ work_with_files.py
@@ -1,28 +1,19 @@
 import sys
-import ntpath
 
+def get_input_file_name(default="input.txt"):
+    """Retrieve the input file name from the command line arguments or use the default."""
+    return sys.argv[1] if len(sys.argv) > 1 else default
 
-def get_input_file_name(argv=sys.argv) -> str:
-    """Get input file name from 1st argument"""
-    if len(argv) > 1:
-        return argv[1]
-    else:
-        sys.stderr.write("there is no input file\n")
+def get_output_file_name(default="output.txt"):
+    """Retrieve the output file name from the command line arguments or use the default."""
+    return sys.argv[2] if len(sys.argv) > 2 else default
 
-
-def get_output_file_name(argv=sys.argv) -> str:
-    """Get output file name. It will generated from input file name or got from 2nd argument"""
-    if len(argv) < 3:
-        return "output_" + ntpath.basename(get_input_file_name())
-    else:
-        return argv[2]
-
-
-def read_file(filename: str) -> list:
-    with open(filename, "r") as file:
+def read_file(file_name):
+    """Read the content of a file and return it as a list of lines."""
+    with open(file_name, "r", encoding="utf-8") as file:
         return file.readlines()
 
-
-def write_to_file(text: list, filename: str):
-    with open(filename, "w") as file:
-        file.writelines(text)
+def write_to_file(lines, file_name):
+    """Write a list of lines to a file."""
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.writelines(lines)
